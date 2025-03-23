55c1525c28ae04beefe6022d944bca21cd8ceb13
Marian Figula <majofigulam3@gmail.com>
2025-03-23 16:25:28 +0100

    feat: added option to change datatype

--- GUI/NumberInput.py
+++ GUI/NumberInput.py
@@ -2,7 +2,9 @@ import customtkinter as ctk
 
 
 class NumberInput(ctk.CTkFrame):
-    def __init__(self, master, min_value=0.0, max_value=1.0, step=0.1, default_value=0.25, disabled_value=0.0, **kwargs):
+    def __init__(self, master, min_value=0.0, max_value=1.0, step=0.1,
+                 default_value=0.25, disabled_value=0.0, data_type=float, **kwargs):
+
         super().__init__(master, **kwargs)
 
         self.min_value = min_value
@@ -10,6 +12,13 @@ class NumberInput(ctk.CTkFrame):
         self.step = step
         self.default_value = default_value
         self.disabled_value = disabled_value
+        self.data_type = data_type
+
+        self.min_value = self.data_type(self.min_value)
+        self.max_value = self.data_type(self.max_value)
+        self.step = self.data_type(self.step)
+        self.default_value = self.data_type(self.default_value)
+        self.disabled_value = self.data_type(self.disabled_value)
 
         self.value = ctk.DoubleVar(value=self.default_value)
 
@@ -24,32 +33,32 @@ class NumberInput(ctk.CTkFrame):
         self.up_button = ctk.CTkButton(self.button_frame, text="▲", width=10, height=10, command=self.increment, font=("Lato", 8))
         self.up_button.pack(pady=(0, 0))
 
-        # Down button to decrease the value
         self.down_button = ctk.CTkButton(self.button_frame, text="▼", width=10, height=10, command=self.decrement, font=("Lato", 8))
         self.down_button.pack(pady=(0, 0))
 
     def validate_input(self, new_value):
         """Validate the input to ensure it's a number between min_value and max_value."""
         try:
-            value = float(new_value)
+            value = self.data_type(new_value)  # Convert to the correct data type
             return self.min_value <= value <= self.max_value
         except ValueError:
             return False
 
     def increment(self):
-        current_value = self.value.get()
+        current_value = self.data_type(self.value.get())  # Convert to the correct data type
         new_value = min(current_value + self.step, self.max_value)
-        self.value.set(round(new_value, 2))
+        self.value.set(self.data_type(new_value))
 
     def decrement(self):
-        current_value = self.value.get()
+        current_value = self.data_type(self.value.get())
         new_value = max(current_value - self.step, self.min_value)
-        self.value.set(round(new_value, 2))
+        self.value.set(self.data_type(new_value))
 
     def get_value(self):
-        return self.value.get()
+        return self.data_type(self.value.get())
 
     def set_value(self, value):
+        value = self.data_type(value)
         if self.min_value <= value <= self.max_value:
             self.value.set(value)
         else:
@@ -62,16 +71,29 @@ class NumberInput(ctk.CTkFrame):
         self.set_value(self.disabled_value if state else self.default_value)
 
     def set_min_value(self, min_value):
-        self.min_value = min_value
+        self.min_value = self.data_type(min_value)
 
     def set_max_value(self, max_value):
-        self.max_value = max_value
+        self.max_value = self.data_type(max_value)
 
     def set_step(self, step):
-        self.step = step
+        self.step = self.data_type(step)
 
     def set_default_value(self, default_value):
-        self.default_value = default_value
+        self.default_value = self.data_type(default_value)
 
     def set_disabled_value(self, disabled_value):
-        self.disabled_value = disabled_value
+        self.disabled_value = self.data_type(disabled_value)
+
+    def set_data_type(self, data_type):
+        """Set the data type (int or float) dynamically."""
+        if data_type not in (int, float):
+            raise ValueError("data_type must be either int or float")
+        self.data_type = data_type
+
+        self.min_value = self.data_type(self.min_value)
+        self.max_value = self.data_type(self.max_value)
+        self.step = self.data_type(self.step)
+        self.default_value = self.data_type(self.default_value)
+        self.disabled_value = self.data_type(self.disabled_value)
+        self.set_value(self.data_type(self.value.get()))
