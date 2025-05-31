ae5118fd6030367d2ed09644d199286a3c2a0d04
Marian Figula <majofigulam3@gmail.com>
2025-05-31 16:17:17 +0200

    fixed problem with models running without console

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+import sys
 import customtkinter as ctk
 from GUI.GUIChooseOptions import GUIChooseOptions
+
+if getattr(sys, 'frozen', False):
+    if sys.stdout is None:
+        sys.stdout = open(os.devnull, 'w')
+    if sys.stderr is None:
+        sys.stderr = open(os.devnull, 'w')
 
 if __name__ == "__main__":
     root = ctk.CTk()
